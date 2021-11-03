9b2f1d03a3a836956b2d1cdc67b6f711b8f83e50
Michael Sales <michael.fernandes.sales@gmail.com>
2021-11-02 22:02:45 -0300

    ocultando tela inicial e mostrando as cartas do jogo

--- assets/scripts/eventos.js
+++ assets/scripts/eventos.js
@@ -1,4 +1,7 @@
 let startButton = document.getElementById("originalStarButton");
 startButton.addEventListener("click", () => {
   onload();
+  ocultar();
 });
+
+const trataCliqueImagem = (e) => {};
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,12 +1,31 @@
 let imagens = [];
 for (let i = 1; i <= 8; i++) {
-  imagens.push(`http://picsum.photos/id/${i}/80`);
+  imagens.push(`http://picsum.photos/id/${i}/250`);
 }
-let fundo = "https://picsum.photos/80?grayscale";
+let fundo = "https://picsum.photos/250?grayscale";
+let cartas = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
 
 const onload = () => {
   let elemImages = document.querySelectorAll("#memoria img");
   elemImages.forEach((img, i) => {
     img.src = fundo;
+  });
+};
+
+function ocultar() {
+  document.getElementById("caixinhaText").style.display = "none";
+}
+
+const play = () => {
+  for (let i = 0; i < cartas.length; i++) {
+    let p = Math.trunc(Math.random() * cartas.length);
+    let aux = cartas[p];
+    cartas[p] = cartas[i];
+    cartas[i] = aux;
+  }
+  let elemImages = document.querySelectorAll("#memoria img");
+  elemImages.forEach((img, i) => {
+    img.onclick = trataCliqueImagem;
+    img.style.opacity = 1;
   });
 };
