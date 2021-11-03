fe62ccdfe2aa21539bef1634e30c3563be02cad2
Michael Sales <michael.fernandes.sales@gmail.com>
2021-11-03 13:52:47 -0300

    tentativa de cartas duplicadas

--- assets/scripts/eventos.js
+++ assets/scripts/eventos.js
@@ -1,23 +1,33 @@
+/*evento click*/
 let startButton = document.getElementById("originalStarButton");
 startButton.addEventListener("click", () => {
   onload();
   ocultar();
-  document.querySelector("#jogar").onclick = play;
+  play();
+  document.querySelector("#originalStarButton").onclick = play;
 });
 
+/*embaralha (tráz o índice duplicado e randomizado"let aux") e distribui as cartas*/
 const play = () => {
   for (let i = 0; i < cartas.length; i++) {
     let p = Math.trunc(Math.random() * cartas.length);
     let aux = cartas[p];
-    cartas[p] = cartas[i];
-    cartas[i] = aux;
+    /*cartas[p] = cartas[i];
+    cartas[i] = aux;*/
   }
+
   let elemImages = document.querySelectorAll("#memoria img");
   elemImages.forEach((img, i) => {
+    console.log("img2", i);
+    console.log("img3", img);
     img.onclick = trataCliqueImagem;
-    img.style.opacity = 1;
   });
 };
-const trataCliqueImagem = (e) => {};
-const p = +e.target.getAttribute("data-valor");
-e.target.src = imagens[p];
+
+/*pega o atributo "data-valor"*/
+const trataCliqueImagem = (e) => {
+  console.log("e", e);
+  const p = +e.target.getAttribute("data-valor");
+  e.target.src = imagens[p];
+  console.log("p", imagens);
+};
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,17 +1,22 @@
 let imagens = [];
-for (let i = 1; i <= 8; i++) {
+
+for (let i = 1; i <= 12; i++) {
   imagens.push(`http://picsum.photos/id/${i}/250`);
 }
 let fundo = "https://picsum.photos/250?grayscale";
-let cartas = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
+let cartas = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6];
 
 const onload = () => {
   let elemImages = document.querySelectorAll("#memoria img");
   elemImages.forEach((img, i) => {
     img.src = fundo;
+    img.setAttribute("data-valor", i);
   });
 };
 
 function ocultar() {
   document.getElementById("caixinhaText").style.display = "none";
 }
+
+/* vira o titulo em 180 graus */
+document.querySelector("#myCard").classList.toggle("flip");
