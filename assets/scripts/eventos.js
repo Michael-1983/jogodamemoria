f51aa87de062f889e8530da4400078110c96af68
Michael Sales <michael.fernandes.sales@gmail.com>
2021-11-04 09:11:29 -0300

    css pronto, faltando embaralhar cartas

--- assets/scripts/eventos.js
+++ assets/scripts/eventos.js
@@ -1,25 +1,24 @@
 /*evento click*/
 let startButton = document.getElementById("originalStarButton");
 startButton.addEventListener("click", () => {
+  play();
   onload();
   ocultar();
-  play();
+
   document.querySelector("#originalStarButton").onclick = play;
 });
 
 /*embaralha (tráz o índice duplicado e randomizado"let aux") e distribui as cartas*/
 const play = () => {
-  for (let i = 0; i < imagens.length; i++) {
+  for (let i = 0; i < cartas.length; i++) {
     let p = Math.trunc(Math.random() * imagens.length);
-    let aux = imagens[p];
-    /*cartas[p] = cartas[i];
-    cartas[i] = aux;*/
+    let aux = cartas[p];
+    cartas[p] = cartas[i];
+    cartas[i] = aux;
   }
 
   let elemImages = document.querySelectorAll("#memoria img");
   elemImages.forEach((img, i) => {
-    console.log("img2", i);
-    console.log("img3", img);
     img.onclick = trataCliqueImagem;
   });
 };
@@ -29,5 +28,4 @@ const trataCliqueImagem = (e) => {
   console.log("e", e);
   const p = +e.target.getAttribute("data-valor");
   e.target.src = imagens[p];
-  console.log("p", imagens);
 };
